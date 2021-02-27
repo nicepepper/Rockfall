97d40db69365dbc474be7e996abf10434d05c172
Rishat Rashitov <wildrage88@gmail.com>
2021-02-27 19:26:40 +0300

    added assets, Smoothfollow.cs script for following camera target

---
+++ Assets/Scripts/ShipThrust.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipThrust : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    public void Update()
+    {
+        var offset = Vector3.forward * Time.deltaTime * speed;
+        this.transform.Translate(offset);
+    }
+}
