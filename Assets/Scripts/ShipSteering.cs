3b027d23e4ac27a78df875448768e72e079c0717
Rishat Rashitov <wildrage88@gmail.com>
2021-02-28 21:04:01 +0300

    Added InputManager, Joystick

---
+++ Assets/Scripts/ShipSteering.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSteering : MonoBehaviour
+{
+    public float turnRate = 6.0f;
+    public float levelDamping = 1.0f;
+
+    private void Update()
+    {
+        var steeringInput = InputManager.instance.steering.delta;
+        var rotation = new Vector2();
+
+        rotation.y = steeringInput.x;
+        rotation.x = steeringInput.y;
+
+        rotation *= turnRate;
+        rotation.x = Mathf.Clamp(rotation.x, -Mathf.PI * 0.9f, Mathf.PI * 0.9f);
+
+        var newOrientation = Quaternion.Euler(rotation);
+        transform.rotation *= newOrientation;
+
+        var levelAngles = transform.eulerAngles;
+        levelAngles.z = 0.0f;
+        var levelOriantation = Quaternion.Euler(levelAngles);
+        
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            levelOriantation,
+            levelDamping * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public RectTransform thumb;
+    private Vector2 originalPosition;
+    private Vector2 originalThumbPosition;
+    public Vector2 delta;
+
+    private void Start()
+    {
+        originalPosition = this.GetComponent<RectTransform>().localPosition;
+        originalThumbPosition = thumb.localPosition;
+        thumb.gameObject.SetActive(false);
+        delta = Vector2.zero;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        thumb.gameObject.SetActive(true);
+        Vector3 worldPoint = new Vector3();
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            this.transform as RectTransform,
+            eventData.position,
+            eventData.enterEventCamera,
+            out worldPoint);
+
+        this.GetComponent<RectTransform>().position = worldPoint;
+        thumb.localPosition = originalThumbPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 worldPoint = new Vector3();
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            this.transform as RectTransform,
+            eventData.position,
+            eventData.enterEventCamera,
+            out worldPoint);
+        
+        thumb.position = worldPoint;
+        var size = GetComponent<RectTransform>().rect.size;
+
+        delta = thumb.localPosition;
+
+        delta.x /= size.x / 2.0f;
+        delta.y /= size.y / 2.0f;
+
+        delta.x = Mathf.Clamp(delta.x, -1.0f, 1.0f);
+        delta.y = Mathf.Clamp(delta.y, -1.0f, 1.0f);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.GetComponent<RectTransform>().localPosition = originalPosition;
+        delta = Vector3.zero;
+        thumb.gameObject.SetActive(false);
+    }
+}
